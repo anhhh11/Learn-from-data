290defe696c30c1af37c46247ffd5de41b6dd4d6
anhhhh.11@gmail.com <anhhhh.11@gmail.com>
2014-07-12 22:52:31 +0700

    update linear hypo

--- hw4.R
+++ hw4.R
@@ -1,25 +1,25 @@
-set.seed(1000)
 x <- seq(-1,1,0.001)
 y <- sin(pi*x)
 org <- data.frame(x1=x,y1=y)
-fit <- lm(y~1)
+fit <- lm(y~x)
 num_sample_data <- 1000
 g_neg <- 0
-var <- rep(0,num_sample_data)
+var <- list()#rep(0,num_sample_data)
 for ( i in seq(1,num_sample_data)){
   x1 <- runif(2,-1,1)
   y1 <- sin(pi*x1)
-  fit_d <- lm(y1~1)
+  fit_d <- lm(y1~x1)
   g_neg <- g_neg + fit_d$coef
   p <- predict(object=fit_d,org)
-  var[i] <- fit_d$coef
+  var[[i]] <- fit_d$coef
 }
 g_neg <- g_neg / num_sample_data
-g_x_neg <- g_neg
+g_x_neg <- g_neg[1] + g_neg[2]*x
+ret_var = c()
 for (i in seq(1,num_sample_data)){
-  var[i] <- mean((var[i]*x-g_x_neg)^2) 
+  ret_var[i] <- mean((var[[i]][2]*x+var[[i]][1]-g_x_neg)^2) 
 }
-var <- mean(var)
+var <- mean(ret_var)
 bias <- mean((g_x_neg - y)^2)
 g_neg
 var
@@ -49,5 +49,13 @@ bias
 # > bias
 # [1] 0.4997849
 
-
-
+# With H: y = ax + b
+# > g_neg
+# (Intercept)           x1 
+# -0.009098882  0.751622699 
+# 
+# > var
+# [1] 1.736339
+# 
+# > bias
+# [1] 0.2100751
